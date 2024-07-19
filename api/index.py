ef0ac7b91820c6abc0c9f5916af6ad734754dc6e
Dridi Slim <dslimmp@gmail.com>
2024-07-19 17:35:23 +0100

    first commi4

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, url_for, redirect, flash, request, jsonify
 import requests
-import geopandas as gpd
+#import geopandas as gpd
 import random
 import pandas as pd
 import plotly.express as px
@@ -13,72 +13,6 @@ app = Flask(__name__, template_folder="templates")
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'thisisasecretkey')
 
 # Function to fetch GeoJSON data
-def fetch_geojson_data():
-    url = 'http://catalog.industrie.gov.tn/dataset/9910662a-4594-453f-a710-b2f339e0d637/resource/1b7e3eba-b178-4902-83db-ef46f26e98a0/download/delegations.geojson'
-    response = requests.get(url)
-    data = response.json()
-    return gpd.GeoDataFrame.from_features(data)
-
-def create_map():
-    # Filter by governorates
-    governorates = ['Manubah', 'Bizerte', 'Zaghouan', 'Siliana', 'Ben Arous', 'Béja', 'Jendouba', 'Le Kef', 'Ariana']
-    geo_df = fetch_geojson_data()
-    filtered_geo_df = geo_df[geo_df['gov_name_f'].isin(governorates)]
-
-    # Generate random values
-    random.seed(42)
-    cereale_codes = [random.randint(1, 4) for _ in range(len(filtered_geo_df))]
-    variete_codes = [random.randint(1, 12) for _ in range(len(filtered_geo_df))]
-    superficies = [random.randint(10, 100) for _ in range(len(filtered_geo_df))]
-    productions = [random.randint(20, 5000) for _ in range(len(filtered_geo_df))]
-
-    # Add new columns
-    filtered_geo_df['code_cereale'] = cereale_codes
-    filtered_geo_df['code_variete'] = variete_codes
-    filtered_geo_df['superficie'] = superficies
-    filtered_geo_df['production'] = productions
-
-    # Map code to text
-    def map_code_to_text(code):
-        return {1: 'BD', 2: 'BT', 3: 'Tr', 4: 'Or'}.get(code, 'Unknown')
-
-    filtered_geo_df['cereale_text'] = filtered_geo_df['code_cereale'].apply(map_code_to_text)
-    
-    # Convert GeoDataFrame to GeoJSON
-    geojson = filtered_geo_df.__geo_interface__
-
-    # Create the choropleth map with Plotly
-    fig = px.choropleth(
-        filtered_geo_df,
-        geojson=geojson,
-        locations=filtered_geo_df.index,
-        color='production',
-        color_continuous_scale='YlGn',
-        hover_data={'cereale_text': True, 'production': True},
-        labels={'production': 'Production', 'cereale_text': 'Cereale Type'},
-        hover_name='gov_name_f', 
-        title='Map of Production by Governorate - Simulated Data',
-        height=895
-    )
-
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(
-        mapbox_style="carto-positron",
-        mapbox_zoom=7,
-        mapbox_center={"lat": 34.0, "lon": 9.0},
-        margin={"r":0,"t":60,"l":0,"b":0},
-        coloraxis_colorbar={
-            'title': 'Production',
-            'tickvals': [min(productions), max(productions)],
-            'ticktext': [min(productions), max(productions)],
-            'x': 1.05,
-            'xanchor': 'left',
-            'y': 0.5,
-            'yanchor': 'middle'
-        }
-    )
-
-    return fig
 
 def create_histogram():
     # Read the CSV file
@@ -139,14 +73,12 @@ def create_bar_chart():
     return fig
 
 # Create Dash applications
-map_app = dash.Dash(__name__, server=app, url_base_pathname='/tunisia_map/')
+
 hist_app = dash.Dash(__name__, server=app, url_base_pathname='/hist/')
 bar_chart_app = dash.Dash(__name__, server=app, url_base_pathname='/bar_chart/')
 
 # Define the layout of the map
-map_app.layout = html.Div([
-    dcc.Graph(id='map', figure=create_map())
-])
+
 
 # Define the layout of the histogram
 hist_app.layout = html.Div([
